88999b1a6771f55c5d74e2954bf568748436dacf
Gzanella1 <giovanizanelladamaia123@gmail.com>
2025-01-03 23:59:01 -0300

    primeiro commit

---
+++ src/main/java/com/giovani/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,14 @@
+package com.giovani.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovani/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovani.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
